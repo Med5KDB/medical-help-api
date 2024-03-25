fa01a62234d4d6a390cb8a539d964d6b1d1c913a
moisemb <moisemb10@gmail.com>
2024-03-25 22:25:26 +0000

    adding crud hospital

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 import { MedicalAssistantModule } from './medical-assistant/medical-assistant.module';
+import { HospitalModule } from './hospital/hospital.module';
 
 @Module({
-  imports: [DoctorModule, PatientModule, MedicalAssistantModule],
+  imports: [DoctorModule, PatientModule, MedicalAssistantModule, HospitalModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
